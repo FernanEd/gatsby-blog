6e633715b21cbebe15ea6abba75676acf98fa76d
fernaned <fernandoed.bm@gmail.com>
2021-05-23 14:49:30 -0500

    rm gatsby boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import Seo from "../components/seo"
+
+const IndexPage = () => {
+  return (
+    <>
+      <Seo title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+
+      <p>
+        <Link to="/about">Go to page 2</Link> <br />
+      </p>
+    </>
+  )
+}
+
+export default IndexPage
